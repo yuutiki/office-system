e1c4ccdd763eeddc456dd78bc3375c0234d75e61
suehisa <bocgod51@gmail.com>
2023-10-10 08:54:45 +0900

    mainte

--- database/seeders/PrefectureSeeder.php
+++ database/seeders/PrefectureSeeder.php
@@ -10,285 +10,286 @@ class PrefectureSeeder extends Seeder
 {
     public function run(): void
     {
+
         Prefecture::create([
-            'code' => '01',
-            'name' => '北海道',
+            'prefecture_code' => '01',
+            'prefecture_name' => '北海道',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '02',
-            'name' => '青森県',
+            'prefecture_code' => '02',
+            'prefecture_name' => '青森県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '03',
-            'name' => '岩手県',
+            'prefecture_code' => '03',
+            'prefecture_name' => '岩手県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '04',
-            'name' => '宮城県',
+            'prefecture_code' => '04',
+            'prefecture_name' => '宮城県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '05',
-            'name' => '秋田県',
+            'prefecture_code' => '05',
+            'prefecture_name' => '秋田県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '06',
-            'name' => '山形県',
+            'prefecture_code' => '06',
+            'prefecture_name' => '山形県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '07',
-            'name' => '福島県',
+            'prefecture_code' => '07',
+            'prefecture_name' => '福島県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '08',
-            'name' => '茨城県',
+            'prefecture_code' => '08',
+            'prefecture_name' => '茨城県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '09',
-            'name' => '栃木県',
+            'prefecture_code' => '09',
+            'prefecture_name' => '栃木県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '10',
-            'name' => '群馬県',
+            'prefecture_code' => '10',
+            'prefecture_name' => '群馬県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '11',
-            'name' => '埼玉県',
+            'prefecture_code' => '11',
+            'prefecture_name' => '埼玉県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '12',
-            'name' => '千葉県',
+            'prefecture_code' => '12',
+            'prefecture_name' => '千葉県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '13',
-            'name' => '東京都',
+            'prefecture_code' => '13',
+            'prefecture_name' => '東京都',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '14',
-            'name' => '神奈川県',
+            'prefecture_code' => '14',
+            'prefecture_name' => '神奈川県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '15',
-            'name' => '新潟県',
+            'prefecture_code' => '15',
+            'prefecture_name' => '新潟県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '16',
-            'name' => '富山県',
+            'prefecture_code' => '16',
+            'prefecture_name' => '富山県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '17',
-            'name' => '石川県',
+            'prefecture_code' => '17',
+            'prefecture_name' => '石川県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '18',
-            'name' => '福井県',
+            'prefecture_code' => '18',
+            'prefecture_name' => '福井県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '19',
-            'name' => '山梨県',
+            'prefecture_code' => '19',
+            'prefecture_name' => '山梨県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '20',
-            'name' => '長野県',
+            'prefecture_code' => '20',
+            'prefecture_name' => '長野県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '21',
-            'name' => '岐阜県',
+            'prefecture_code' => '21',
+            'prefecture_name' => '岐阜県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '22',
-            'name' => '静岡県',
+            'prefecture_code' => '22',
+            'prefecture_name' => '静岡県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '23',
-            'name' => '愛知県',
+            'prefecture_code' => '23',
+            'prefecture_name' => '愛知県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '24',
-            'name' => '三重県',
+            'prefecture_code' => '24',
+            'prefecture_name' => '三重県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '25',
-            'name' => '滋賀県',
+            'prefecture_code' => '25',
+            'prefecture_name' => '滋賀県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '26',
-            'name' => '京都府',
+            'prefecture_code' => '26',
+            'prefecture_name' => '京都府',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '27',
-            'name' => '大阪府',
+            'prefecture_code' => '27',
+            'prefecture_name' => '大阪府',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '28',
-            'name' => '兵庫県',
+            'prefecture_code' => '28',
+            'prefecture_name' => '兵庫県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '29',
-            'name' => '奈良県',
+            'prefecture_code' => '29',
+            'prefecture_name' => '奈良県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '30',
-            'name' => '和歌山県',
+            'prefecture_code' => '30',
+            'prefecture_name' => '和歌山県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '31',
-            'name' => '鳥取県',
+            'prefecture_code' => '31',
+            'prefecture_name' => '鳥取県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '32',
-            'name' => '島根県',
+            'prefecture_code' => '32',
+            'prefecture_name' => '島根県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '33',
-            'name' => '岡山県',
+            'prefecture_code' => '33',
+            'prefecture_name' => '岡山県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '34',
-            'name' => '広島県',
+            'prefecture_code' => '34',
+            'prefecture_name' => '広島県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '35',
-            'name' => '山口県',
+            'prefecture_code' => '35',
+            'prefecture_name' => '山口県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '36',
-            'name' => '徳島県',
+            'prefecture_code' => '36',
+            'prefecture_name' => '徳島県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '37',
-            'name' => '香川県',
+            'prefecture_code' => '37',
+            'prefecture_name' => '香川県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '38',
-            'name' => '愛媛県',
+            'prefecture_code' => '38',
+            'prefecture_name' => '愛媛県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '39',
-            'name' => '高知県',
+            'prefecture_code' => '39',
+            'prefecture_name' => '高知県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '40',
-            'name' => '福岡県',
+            'prefecture_code' => '40',
+            'prefecture_name' => '福岡県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '41',
-            'name' => '佐賀県',
+            'prefecture_code' => '41',
+            'prefecture_name' => '佐賀県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '42',
-            'name' => '長崎県',
+            'prefecture_code' => '42',
+            'prefecture_name' => '長崎県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '43',
-            'name' => '熊本県',
+            'prefecture_code' => '43',
+            'prefecture_name' => '熊本県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '44',
-            'name' => '大分県',
+            'prefecture_code' => '44',
+            'prefecture_name' => '大分県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '45',
-            'name' => '宮崎県',
+            'prefecture_code' => '45',
+            'prefecture_name' => '宮崎県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '46',
-            'name' => '鹿児島県',
+            'prefecture_code' => '46',
+            'prefecture_name' => '鹿児島県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
         Prefecture::create([
-            'code' => '47',
-            'name' => '沖縄県',
+            'prefecture_code' => '47',
+            'prefecture_name' => '沖縄県',
             'created_at' => now(),
             'updated_at' => now()
         ]);
